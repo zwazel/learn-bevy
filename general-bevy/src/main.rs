71aa1dafdaf41d826ed3c4b45d00217e8a178408
Zwazel <zwazel@hotmail.com>
2022-04-02 11:51:07 +0200

    created our own resource, timer!

--- general-bevy/src/main.rs
+++ general-bevy/src/main.rs
@@ -7,10 +7,6 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 // startup system - runs only once, before everything else
 fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
@@ -18,26 +14,39 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
+// our own resource
+struct GreetTimer(Timer);
+
+// functions are systems!
 // this system runs on all entities with the "Person" and "Name" component
 // The query can be interpreted as "iterate over every Name component for entities that also have a Person component"
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+// Time is a resource! Res<T> for read, ResMut<T> for write access!
+fn greet_people(
+    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
+// components!
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
+// Plugin!
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        // the reason we call from_seconds with the true flag is to make the timer repeat itself
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
