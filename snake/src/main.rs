4eafde0cda8865d868848a03c3722b45bfbda8de
Zwazel <zwazel@hotmail.com>
2022-04-03 01:53:28 +0200

    oh shit i was able to do the timer shit!

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -6,9 +6,25 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .insert_resource(Global {
+            grid_size: Vec2::new(500.0, 500.0),
+            scale: Vec2::new(10.0, 10.0),
+        })
+        .insert_resource(MoveTimer {
+            timer: Timer::from_seconds(0.2, true),
+        })
         .add_system_set(SystemSet::new()
             .with_system(snake_movement))
         .run();
+}
+
+struct Global {
+    grid_size: Vec2,
+    scale: Vec2,
+}
+
+struct MoveTimer {
+    timer: Timer,
 }
 
 #[derive(Component)]
@@ -17,17 +33,21 @@ struct SnakePiece;
 #[derive(Component)]
 struct SnakeHead;
 
-fn setup(mut commands: Commands) {
+#[derive(Component)]
+struct Direction {
+    dir: Vec2,
+}
+
+fn setup(mut commands: Commands, global_settings: Res<Global>) {
     // cameras
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
 
-    let pos = (10.0, 10.0);
     // setup the snake
     commands.spawn_bundle(SpriteBundle {
         transform: Transform {
-            translation: Vec3::new(pos.0, pos.1, 0.0),
-            scale: Vec3::new(10.0, 10.0, 0.0),
+            translation: Vec3::new(global_settings.grid_size.x / 2.0, global_settings.grid_size.y / 2.0, 0.0),
+            scale: Vec3::new(global_settings.scale.x, global_settings.scale.y, 0.0),
             ..Default::default()
         },
         sprite: Sprite {
@@ -37,30 +57,62 @@ fn setup(mut commands: Commands) {
         ..Default::default()
     })
         .insert(SnakePiece)
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Direction {
+            dir: Vec2::new(1.0, 0.0),
+        });
+
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(global_settings.grid_size.x / 2.0, global_settings.grid_size.y / 2.0, 0.0),
+            scale: Vec3::new(10.0, 10.0, 0.0),
+            ..Default::default()
+        },
+        sprite: Sprite {
+            color: Color::rgb(1.0, 0.5, 1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+        .insert(SnakePiece);
 }
 
-fn snake_movement(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&SnakePiece, With<SnakeHead>, &mut Transform)>) {
-    let (snake_head, _, mut transform) = query.single_mut();
+fn snake_movement(keyboard_input: Res<Input<KeyCode>>, global_settings: Res<Global>, time: Res<Time>, mut move_timer: ResMut<MoveTimer>, mut query: Query<(With<SnakeHead>, &mut Direction, &mut Transform)>) {
+    let (_, mut direction, mut transform) = query.single_mut(); // this panics if there are multiple query results!
 
-    let mut direction = Vec2::new(0.0, 0.0);
     if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-        direction.x -= 1.0;
+        direction.dir.x = -1.0;
+        direction.dir.y = 0.0;
+    } else if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+        direction.dir.x = 1.0;
+        direction.dir.y = 0.0;
+    } else if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+        direction.dir.y = 1.0;
+        direction.dir.x = 0.0;
+    } else if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+        direction.dir.y = -1.0;
+        direction.dir.x = 0.0;
     }
 
-    if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-        direction.x += 1.0;
-    }
+    if move_timer.timer.tick(time.delta()).just_finished() {
+        println!("{:?}", transform.translation);
+        let translation = &mut transform.translation;
+        translation.x += direction.dir.x * global_settings.scale.x;
+        translation.y += direction.dir.y * global_settings.scale.y;
 
-    if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-        direction.y += 1.0;
+        // wrap around the screen
+        if (translation.x) > global_settings.grid_size.x {
+            translation.x = 0.0 + global_settings.scale.x;
+            println!("wrapped right to left");
+        } else if translation.x < 0.0 {
+            translation.x = global_settings.grid_size.x - global_settings.scale.x;
+            println!("wrapped left to right");
+        } else if (translation.y) > global_settings.grid_size.y {
+            translation.y = 0.0 + global_settings.scale.y;
+            println!("wrapped up to down");
+        } else if translation.y < 0.0 {
+            translation.y = global_settings.grid_size.y - global_settings.scale.y;
+            println!("wrapped down to up");
+        }
     }
-
-    if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-        direction.y -= 1.0;
-    }
-
-    let translation = &mut transform.translation;
-    translation.x += direction.x;
-    translation.y += direction.y;
 }
