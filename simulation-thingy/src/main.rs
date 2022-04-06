41f235e64ac4294bc11ad7cedfc32eeb8aa845cc
Zwazel <zwazel@hotmail.com>
2022-04-06 15:42:31 +0200

    jawooohl systemlabels!

--- simulation-thingy/src/main.rs
+++ simulation-thingy/src/main.rs
@@ -3,14 +3,27 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(hunger_system)
-        .add_system(print_hunger_status_system)
+        .add_system(hunger_system
+            .label(DebugOrNot::NotDebug)
+            .before(DebugOrNot::Debug)
+        )
+        .add_system(print_hunger_status_system
+            .label(DebugOrNot::Debug)
+            .after(DebugOrNot::NotDebug)
+        )
         .run();
 }
 
 #[derive(Component)]
 struct Hunger(f32);
+
+#[derive(SystemLabel, Clone, Hash, Debug, PartialEq, Eq)]
+enum DebugOrNot {
+    Debug,
+    NotDebug,
+}
 
 fn setup(mut commands: Commands) {
     commands.spawn().insert(Hunger(0.0));
@@ -18,12 +31,13 @@ fn setup(mut commands: Commands) {
 }
 
 fn hunger_system(mut query: Query<&mut Hunger>) {
-    for hunger in query.iter_mut() {
+    for mut hunger in query.iter_mut() {
         hunger.0 += 0.001;
+        println!("system")
     }
 }
 
-fn print_hunger_status_system(mut query: Query<&Hunger>) {
+fn print_hunger_status_system(query: Query<&Hunger>) {
     for hunger in query.iter() {
         println!("Hunger: {}", hunger.0);
     }
