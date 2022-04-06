dab27fd5bc6d9d4f85784071cd393ad4fed14df6
Zwazel <zwazel@hotmail.com>
2022-04-06 11:47:47 +0200

    camera

--- autobattler-but-bevy/src/main.rs
+++ autobattler-but-bevy/src/main.rs
@@ -1,8 +1,13 @@
 use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use bevy::prelude::{App, Commands, OrthographicCameraBundle};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    // setup 2d camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
