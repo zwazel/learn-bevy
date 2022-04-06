e1704cd86a787ad67b71de835ae55f0b4e3b9ead
Zwazel <zwazel@hotmail.com>
2022-04-06 16:35:09 +0200

    health system, in theory :)

--- simulation-thingy/src/main.rs
+++ simulation-thingy/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
             .label(DebugOrNot::Debug)
             .after(DebugOrNot::NotDebug)
         )
-        .add_system(health_check_system
+        .add_system(hunger_check_system
             .label(DebugOrNot::NotDebug)
             .before(DebugOrNot::Debug)
         )
@@ -24,10 +24,13 @@ fn main() {
 struct Hunger(f32, f32); // first value is hunger, second is max hunger. if max hunger is reached they die
 
 #[derive(Component)]
-struct HungerModifier(f32);
+struct HungerModifier(f32); // changes how quickly they get hungry. + value means faster more hunger, - value means less fast hunger
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Component)]
+struct Health(f32);
 
 #[derive(SystemLabel, Clone, Hash, Debug, PartialEq, Eq)]
 enum DebugOrNot {
@@ -36,11 +39,29 @@ enum DebugOrNot {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn().insert(Hunger(0.0, 1.0)).insert(Name("Fritz".to_string()));
-    commands.spawn().insert(Hunger(0.5, 12.0)).insert(Name("Hans".to_string())).insert(HungerModifier(0.1));
+    commands.spawn()
+        .insert(Hunger(0.0, 100.0))
+        .insert(Name("Fritz".to_string()))
+        .insert(Health(100.0))
+    ;
+    commands.spawn()
+        .insert(Hunger(0.0, 120.0))
+        .insert(Name("Hans".to_string()))
+        .insert(HungerModifier(1.0))
+        .insert(Health(100.0))
+    ;
 }
 
-fn health_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger, &Name)>) {
+fn health_check_system(mut commands: Commands, mut query: Query<(Entity, &Health, &Name)>) {
+    for (entity, health, name) in &mut query.iter() {
+        if health.0 <= 0.0 {
+            println!("{} died", name.0);
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn hunger_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger, &Name)>) {
     for (entity, hunger, name) in query.iter_mut() {
         if hunger.0 >= hunger.1 {
             println!("{} died of hunger", name.0);
@@ -51,9 +72,9 @@ fn health_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger
 
 fn hunger_system(mut query: Query<(&mut Hunger, Option<&HungerModifier>)>) {
     for (mut hunger, hunger_modifier) in query.iter_mut() {
-        let mut difference = 0.001;
+        let mut difference = 1.0;
         match hunger_modifier {
-            Some(modifier) => if difference + modifier.0 > 0.0 {
+            Some(modifier) => if difference + modifier.0 >= difference {
                 difference += modifier.0;
             },
             None => (),
