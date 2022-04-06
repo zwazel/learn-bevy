c4bd393d83ff9a8d7f1b853ca8b4ce487eefe962
Zwazel <zwazel@hotmail.com>
2022-04-06 16:04:32 +0200

    name component, now trying to "kill" them once they are too hungry

--- simulation-thingy/src/main.rs
+++ simulation-thingy/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::app::App;
 use bevy::prelude::*;
+use bevy::render::render_resource::std140::AsStd140;
 
 fn main() {
     App::new()
@@ -19,6 +20,9 @@ fn main() {
 #[derive(Component)]
 struct Hunger(f32);
 
+#[derive(Component)]
+struct Name(String);
+
 #[derive(SystemLabel, Clone, Hash, Debug, PartialEq, Eq)]
 enum DebugOrNot {
     Debug,
@@ -26,18 +30,26 @@ enum DebugOrNot {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn().insert(Hunger(0.0));
-    commands.spawn().insert(Hunger(69.0));
+    commands.spawn().insert(Hunger(0.0)).insert(Name("Fritz".to_string()));
+    commands.spawn().insert(Hunger(0.5)).insert(Name("Hans".to_string()));
 }
 
-fn hunger_system(mut query: Query<&mut Hunger>) {
-    for mut hunger in query.iter_mut() {
-        hunger.0 += 0.001;
+fn hunger_system(mut commands: Commands, mut query: Query<(&mut Hunger, &Name)>) {
+    // Loop through entities and increase hunger
+
+    for (mut hunger, name) in query.iter_mut() {
+        hunger.0 += 0.1;
+
+        if hunger.0 >= 1.0 {
+            hunger.0 = 0.0;
+            println!("{} is hungry", name.0);
+            // commands.entity(entity).despawn();
+        }
     }
 }
 
-fn print_hunger_status_system(query: Query<&Hunger>) {
-    for hunger in query.iter() {
-        println!("Hunger: {}", hunger.0);
+fn print_hunger_status_system(query: Query<(&Hunger, &Name)>) {
+    for (hunger, name) in query.iter() {
+        println!("Hunger of {}: {}", name.0, hunger.0);
     }
 }
