1c6aca91acfd5e664c99ef832db2be327a836d48
Zwazel <zwazel@hotmail.com>
2022-04-06 16:26:37 +0200

    das isch toll :D

--- simulation-thingy/src/main.rs
+++ simulation-thingy/src/main.rs
@@ -37,7 +37,7 @@ enum DebugOrNot {
 
 fn setup(mut commands: Commands) {
     commands.spawn().insert(Hunger(0.0, 1.0)).insert(Name("Fritz".to_string()));
-    commands.spawn().insert(Hunger(0.5, 12.0)).insert(Name("Hans".to_string()));
+    commands.spawn().insert(Hunger(0.5, 12.0)).insert(Name("Hans".to_string())).insert(HungerModifier(0.1));
 }
 
 fn health_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger, &Name)>) {
@@ -49,11 +49,17 @@ fn health_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger
     }
 }
 
-fn hunger_system(mut query: Query<&mut Hunger>) {
-    // Loop through entities and increase hunger
+fn hunger_system(mut query: Query<(&mut Hunger, Option<&HungerModifier>)>) {
+    for (mut hunger, hunger_modifier) in query.iter_mut() {
+        let mut difference = 0.001;
+        match hunger_modifier {
+            Some(modifier) => if difference + modifier.0 > 0.0 {
+                difference += modifier.0;
+            },
+            None => (),
+        }
 
-    for mut hunger in query.iter_mut() {
-        hunger.0 += 0.1;
+        hunger.0 += difference;
     }
 }
 
