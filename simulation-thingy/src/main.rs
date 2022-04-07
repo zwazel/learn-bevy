9e41a43240771788798f5f7c7a425a43fbff3732
Zwazel <zwazel@hotmail.com>
2022-04-07 15:16:44 +0200

    ich versueche sache us

--- simulation-thingy/src/main.rs
+++ simulation-thingy/src/main.rs
@@ -1,10 +1,20 @@
 use bevy::app::App;
 use bevy::prelude::*;
 
+const WORLD_HEIGHT: u32 = 100;
+const WORLD_WIDTH: u32 = 100;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Simulation!".to_string(),
+            width: 750.0,
+            height: 750.0,
+            ..Default::default()
+        })
+        .add_startup_system(setup_camera)
+        .add_startup_system(setup_entities)
         .add_system(hunger_system
             .label(DebugOrNot::NotDebug)
             .before(DebugOrNot::Debug)
@@ -29,6 +39,7 @@ fn main() {
             .label(DebugOrNot::Debug)
             .after(DebugOrNot::NotDebug)
         )
+        .add_plugins(DefaultPlugins)
         .run();
 }
 
@@ -47,24 +58,51 @@ struct HealthModifier(f32);
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
 #[derive(SystemLabel, Clone, Hash, Debug, PartialEq, Eq)]
 enum DebugOrNot {
     Debug,
     NotDebug,
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn()
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn setup_entities(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1.0, 0.0, 0.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
         .insert(Hunger(0.0, 100.0))
         .insert(Name("Fritz".to_string()))
         .insert(Health(100.0, 100.0))
-    ;
-    commands.spawn()
-        .insert(Hunger(0.0, 120.0))
-        .insert(Name("Hans".to_string()))
-        .insert(HungerModifier(-1.0)) // dont get hungry
-        .insert(Health(100.0, 100.0))
-        .insert(HealthModifier(-1.0)) // but lose health
+        .insert(Position { x: 10, y: 10 })
+        .insert(Size::square(1.0))
     ;
 }
 
@@ -102,20 +140,44 @@ fn hunger_check_system(mut commands: Commands, mut query: Query<(Entity, &Hunger
     }
 }
 
-fn hunger_system(mut query: Query<(&mut Hunger, Option<&HungerModifier>)>) {
+fn hunger_system(mut query: Query<(&mut Hunger, &HungerModifier)>) {
     for (mut hunger, hunger_modifier) in query.iter_mut() {
-        let mut difference = 1.0;
-        match hunger_modifier {
-            Some(modifier) => difference += modifier.0,
-            None => (),
+        if hunger.0 + hunger_modifier.0 > 0.0 {
+            hunger.0 += hunger_modifier.0;
+        } else {
+            hunger.0 = 0.0;
         }
-
-        hunger.0 += difference;
     }
 }
 
 fn print_hunger_status_system(query: Query<(&Hunger, &Name)>) {
     for (hunger, name) in query.iter() {
         println!("Hunger of {}: {}", name.0, hunger.0);
+    }
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / WORLD_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / WORLD_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, WORLD_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, WORLD_HEIGHT as f32),
+            0.0,
+        );
     }
 }
