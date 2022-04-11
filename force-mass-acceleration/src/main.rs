d52bcb33e0841e95aef0f79c2be28284a49c6d39
Zwazel <zwazel@hotmail.com>
2022-04-11 18:57:51 +0200

    it no work

--- force-mass-acceleration/src/main.rs
+++ force-mass-acceleration/src/main.rs
@@ -6,7 +6,6 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_resource(PlanetsID(Vec::new()))
         .add_startup_system(setup_camera)
         .add_startup_system(setup_entities)
         .add_system(update_velocity)
@@ -15,12 +14,9 @@ fn main() {
 }
 
 const UNIVERSE_GRAVITATIONAL_CONSTANT: f32 = 0.0000000000667408;
-const SCALE_CONST: f32 = 10.0;
 
 #[derive(Component)]
 struct Movable;
-
-struct PlanetsID(Vec<u128>);
 
 #[derive(Component)]
 struct Planet {
@@ -34,10 +30,6 @@ fn update_position(
     time: Res<Time>,
     mut planets: Query<(&Planet, &mut Transform, Option<&Movable>)>,
 ) {
-    fn multiply_by_scalar(vec: Vec3, scalar: f32) -> Vec3 {
-        vec * scalar
-    }
-
     for (planet, mut transform, movable) in planets.iter_mut() {
         if let Some(_) = movable {
             transform.translation += planet.velocity * time.delta().as_secs_f32();
@@ -53,22 +45,27 @@ fn update_velocity(
         v.x * v.x + v.y * v.y + v.z * v.z
     }
 
-    let planets = planets.iter_mut();
-    let other_planets = planets.;
+    let mut planets_velocities = Vec::new();
 
-    for (entity, mut planet, planet_transform, planet_movable) in planets {
+    for (entity, planet, planet_transform, planet_movable) in planets.iter() {
         if let Some(_) = planet_movable {
-            for (other_entity, other_planet, other_planet_transform, _) in other_planets {
+            let mut current_velocity = planet.initial_velocity;
+            for (other_entity, other_planet, other_planet_transform, _) in planets.iter() {
                 if entity.id() != other_entity.id() {
                     let distance = other_planet_transform.translation - planet_transform.translation;
                     let distance_squared = magnitude_squared(other_planet_transform.translation - planet_transform.translation);
                     let force_direction = distance.normalize();
                     let force = force_direction * UNIVERSE_GRAVITATIONAL_CONSTANT * planet.mass * other_planet.mass / distance_squared;
                     let acceleration = force / planet.mass;
-                    planet.velocity += acceleration * time.delta().as_secs_f32();
+                    current_velocity += acceleration * time.delta().as_secs_f32();
                 }
             }
+            planets_velocities.push((entity, current_velocity));
         }
+    }
+
+    for (entity, current_velocity) in planets_velocities {
+        planets.get_mut(entity).unwrap().velocity = current_velocity;
     }
 }
 
@@ -78,6 +75,7 @@ fn setup_camera(mut commands: Commands) {
 
 fn setup_entities(mut commands: Commands) {
     let radius = 100.0;
+
     commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
             // blue planet
