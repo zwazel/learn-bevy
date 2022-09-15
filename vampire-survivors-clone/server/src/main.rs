7663e98314e8b1676efd5ef35a8023d711bba00d
Zwazel <zwazel@hotmail.com>
2022-09-15 13:42:22 +0200

    I CANNOT USE LOCALHOST

--- vampire-survivors-clone/server/src/main.rs
+++ vampire-survivors-clone/server/src/main.rs
@@ -1,1 +1,37 @@
-fn main() {}
+use std::net::{SocketAddr, UdpSocket};
+use std::time::SystemTime;
+
+use log::trace;
+use renet::{RenetConnectionConfig, RenetServer, ServerAuthentication, ServerConfig};
+
+use store::{HOST, PORT};
+
+pub const PROTOCOL_ID: u64 = 6969;
+
+fn main() {
+    println!("{}", HOST);
+    println!("{}", PORT);
+    let address = format!("{}:{}", HOST, PORT);
+    println!("{}", address);
+
+    let server_addr: SocketAddr = format!("{}:{}", HOST, PORT)
+        .parse()
+        .unwrap();
+
+    let mut server: RenetServer = RenetServer::new(
+        // Pass the current time to renet, so it can use it to order messages
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap(),
+        // Pass a server configuration specifying that we want to allow only 2 clients to connect
+        // and that we don't want to authenticate them. Everybody is welcome!
+        ServerConfig::new(2, PROTOCOL_ID, server_addr, ServerAuthentication::Unsecure),
+        // Pass the default connection configuration. This will create a reliable, unreliable and blocking channel.
+        // We only actually need the reliable one, but we can just not use the other two.
+        RenetConnectionConfig::default(),
+        UdpSocket::bind(server_addr).unwrap(),
+    )
+        .unwrap();
+
+    trace!("🕹  TicTacTussle server listening on {}", server_addr);
+}
---
+++ vampire-survivors-clone/store/src/lib.rs
@@ -0,0 +1,2 @@
+pub const PORT: i32 = 5555;
+pub const HOST: &str = "127.0.0.1";
