6e91bf34ccba1763fd81f17eb06876f4098fba6d
Tim Irmler <zwazel@hotmail.com>
2022-10-18 10:46:26 +0200

    server and client running!

--- lockstep-multiplayer-experimenting/src/bin/main.rs
+++ lockstep-multiplayer-experimenting/src/bin/main.rs
@@ -1,13 +1,13 @@
 use std::fmt::{Debug, Display, Formatter};
-use std::net::UdpSocket;
+use std::net::{SocketAddr, UdpSocket};
 use std::time::SystemTime;
 use bevy::app::{App, AppExit, CoreStage};
 use bevy::DefaultPlugins;
 use bevy::prelude::{ClearColor, Color, default, EventReader, ResMut, WindowDescriptor};
 use bevy::window::WindowSettings;
-use bevy_renet::{RenetClientPlugin, run_if_client_connected};
-use renet::{ClientAuthentication, NETCODE_USER_DATA_BYTES, RenetClient, RenetError};
-use lockstep_multiplayer_experimenting::{client_connection_config, PORT, PROTOCOL_ID, translate_host, translate_port, VERSION};
+use bevy_renet::{RenetClientPlugin, RenetServerPlugin, run_if_client_connected};
+use renet::{ClientAuthentication, NETCODE_USER_DATA_BYTES, RenetClient, RenetError, RenetServer, ServerAuthentication, ServerConfig};
+use lockstep_multiplayer_experimenting::{AMOUNT_PLAYERS, client_connection_config, PORT, PROTOCOL_ID, server_connection_config, translate_host, translate_port, VERSION};
 
 enum NetworkType {
     Client,
@@ -33,11 +33,16 @@ impl Display for NetworkType {
 }
 
 fn resolve_type(my_type: &str) -> NetworkType {
-    match my_type {
+    let my_type = my_type.to_lowercase();
+    match my_type.as_str() {
         "client" => NetworkType::Client,
         "server" => NetworkType::Server,
         _ => NetworkType::Client,
     }
+}
+
+fn translate_amount_players(amount_players: &str) -> usize {
+    amount_players.parse::<usize>().unwrap_or(AMOUNT_PLAYERS)
 }
 
 fn main() {
@@ -47,6 +52,7 @@ fn main() {
     let mut host = "127.0.0.1";
     let mut port = PORT;
     let mut my_type = NetworkType::Client;
+    let mut amount_of_players = AMOUNT_PLAYERS;
     match args.len() {
         2 => {
             my_type = resolve_type(&args[1]);
@@ -74,37 +80,63 @@ fn main() {
 
             println!("Type has been set to: {}, Username has been set to: {}, Host has been set to: {}, Port has been set to: {}", my_type, username, host, port);
         }
+        6 => {
+            my_type = resolve_type(&args[1]);
+            username = args[2].clone();
+            host = translate_host(&args[3], "");
+            port = translate_port(&args[4]);
+            amount_of_players = translate_amount_players(&args[5]);
+
+            println!("Type has been set to: {}, Username has been set to: {}, Host has been set to: {}, Port has been set to: {}, Amount of players has been set to: {}", my_type, username, host, port, amount_of_players);
+        }
         _ => {
-            println!("Usage: client [username] [host] [port]");
-            println!("Default values: username: {}, host: {}, port: {}", username, host, port);
+            println!("Usage: client [ClientType: server/client] [username] [host] [port] [amount of players]");
+            println!("Default values:\n\tClientType: {}\n\tusername: {}\n\thost: {}\n\tport: {}\n\tamount players: {}", my_type, username, host, port, amount_of_players);
         }
     }
 
     println!("Version: {}", VERSION);
 
-    App::new()
-        .insert_resource(WindowDescriptor {
-            title: format!("Lockstep Experimenting <{}>", username),
-            width: 480.0,
-            height: 540.0,
-            ..default()
-        })
-        .insert_resource(WindowSettings {
-            ..default()
-        })
-        .insert_resource(new_renet_client(&username, host, port))
+    let mut app = App::new();
+    app.insert_resource(WindowDescriptor {
+        title: format!("Lockstep Experimenting <{}>", username),
+        width: 480.0,
+        height: 540.0,
+        ..default()
+    });
+    app.insert_resource(WindowSettings {
+        ..default()
+    });
 
-        .add_plugins(DefaultPlugins)
-        .add_plugin(RenetClientPlugin)
+    app.add_plugins(DefaultPlugins);
 
-        .add_system(panic_on_error_system)
+    app.add_system(panic_on_error_system);
 
-        .add_system_to_stage(CoreStage::Last, disconnect)
-
-        .run();
+    match my_type {
+        NetworkType::Client => {
+            app.add_plugin(RenetClientPlugin);
+            app.add_system_to_stage(CoreStage::Last, disconnect);
+            app.insert_resource(new_renet_client(&username, host, port))
+        }
+        NetworkType::Server => {
+            app.add_plugin(RenetServerPlugin);
+            app.insert_resource(new_renet_server(amount_of_players, host, port))
+        }
+    }.run();
 }
 
 ////////// RENET NETWORKING //////////
+fn new_renet_server(amount_of_player: usize, host: &str, port: i32) -> RenetServer {
+    let server_addr: SocketAddr = format!("{}:{}", host, port)
+        .parse()
+        .unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let connection_config = server_connection_config();
+    let server_config = ServerConfig::new(amount_of_player, PROTOCOL_ID, server_addr, ServerAuthentication::Unsecure);
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    RenetServer::new(current_time, server_config, connection_config, socket).unwrap()
+}
+
 fn new_renet_client(username: &String, host: &str, port: i32) -> RenetClient {
     let server_addr = format!("{}:{}", host, port).parse().unwrap();
     let socket = UdpSocket::bind(format!("0.0.0.0:0")).unwrap();
