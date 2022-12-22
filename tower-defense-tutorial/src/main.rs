5161a7b1cc012ae623064722d2084a3494ea85da
Tim Irmler <zwazel@hotmail.com>
2022-12-22 15:17:46 +0100

    window + bevy

--- tower-defense-tutorial/src/main.rs
+++ tower-defense-tutorial/src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Bevy Tower Defense".to_string(),
+                width: WIDTH,
+                height: HEIGHT,
+                resizable: false,
+                ..Default::default()
+            },
+            ..Default::default()
+        }))
+        .run();
 }
