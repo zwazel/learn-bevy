56115b915f15b060ba703cc1882219faa7d9a7ae
Tim Irmler <zwazel@hotmail.com>
2023-02-03 14:53:38 +0100

    spawn and despawn bullets

--- tower-defense-tutorial/src/main.rs
+++ tower-defense-tutorial/src/main.rs
@@ -1,5 +1,7 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
-use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
@@ -7,8 +9,6 @@ pub const WIDTH: f32 = 1280.0;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-        .add_startup_system(spawn_basic_scene)
-        .add_startup_system(spawn_camera)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
                 title: "Bevy Tower Defense".to_string(),
@@ -20,8 +20,28 @@ fn main() {
             ..Default::default()
         }))
         // inspector Setup
-        .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(WorldInspectorPlugin)
+        .register_type::<Tower>()
+
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_camera)
+
+        .add_system(tower_shooting)
+        .add_system(bullet_despawn)
+
         .run();
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Tower {
+    shooting_timer: Timer,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -38,19 +58,65 @@ fn spawn_basic_scene(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // Plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    })
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..default()
+        })
         .insert(Name::new("Ground"));
 
     // Cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    })
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Tower {
+            shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        })
         .insert(Name::new("Tower"));
+}
+
+fn tower_shooting(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut towers: Query<&mut Tower>,
+    time: Res<Time>,
+) {
+    for mut tower in towers.iter_mut() {
+        tower.shooting_timer.tick(time.delta());
+        if tower.shooting_timer.just_finished() {
+            let spawn_transform = Transform::from_xyz(0.0, 0.7, 0.6)
+                .with_rotation(Quat::from_rotation_y(-PI / 2.0));
+
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+                    material: materials.add(Color::rgb(0.87, 0.44, 0.42).into()),
+                    transform: spawn_transform,
+                    ..default()
+                })
+                .insert(Lifetime {
+                    timer: Timer::from_seconds(0.5, TimerMode::Once),
+                })
+                .insert(Name::new("Bullet"));
+        }
+    }
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>,
+) {
+    for (entity,mut lifetime) in bullets.iter_mut() {
+        lifetime.timer.tick(time.delta());
+        if lifetime.timer.just_finished() {
+            commands.entity(entity).despawn()
+        }
+    }
 }
